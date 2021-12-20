ecc999905a3690bfafab80d0560f738b46ad7925
Fernando-Jacob <zfnsjsyw@sina.com>
2021-12-20 04:09:25 -0500

    test ignoring build

---
+++ build/CMakeFiles/CS_SLAM.dir/DependInfo.cmake
@@ -0,0 +1,44 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/fernando/Code/CS_SLAM/src/ASEKF.cpp" "/home/fernando/Code/CS_SLAM/build/CMakeFiles/CS_SLAM.dir/src/ASEKF.cpp.o"
+  "/home/fernando/Code/CS_SLAM/src/EKF.cpp" "/home/fernando/Code/CS_SLAM/build/CMakeFiles/CS_SLAM.dir/src/EKF.cpp.o"
+  "/home/fernando/Code/CS_SLAM/src/KeyFrame.cpp" "/home/fernando/Code/CS_SLAM/build/CMakeFiles/CS_SLAM.dir/src/KeyFrame.cpp.o"
+  "/home/fernando/Code/CS_SLAM/src/MapPoint.cpp" "/home/fernando/Code/CS_SLAM/build/CMakeFiles/CS_SLAM.dir/src/MapPoint.cpp.o"
+  "/home/fernando/Code/CS_SLAM/src/MeasurementPackage.cpp" "/home/fernando/Code/CS_SLAM/build/CMakeFiles/CS_SLAM.dir/src/MeasurementPackage.cpp.o"
+  "/home/fernando/Code/CS_SLAM/src/RandomVector.cpp" "/home/fernando/Code/CS_SLAM/build/CMakeFiles/CS_SLAM.dir/src/RandomVector.cpp.o"
+  "/home/fernando/Code/CS_SLAM/src/ScanFormer.cpp" "/home/fernando/Code/CS_SLAM/build/CMakeFiles/CS_SLAM.dir/src/ScanFormer.cpp.o"
+  "/home/fernando/Code/CS_SLAM/src/System.cpp" "/home/fernando/Code/CS_SLAM/build/CMakeFiles/CS_SLAM.dir/src/System.cpp.o"
+  "/home/fernando/Code/CS_SLAM/src/Utils.cpp" "/home/fernando/Code/CS_SLAM/build/CMakeFiles/CS_SLAM.dir/src/Utils.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "CERES_EXPORT_INTERNAL_SYMBOLS"
+  "COMPILEDWITHC11"
+  "CS_SLAM_EXPORTS"
+  "GFLAGS_IS_A_DLL=0"
+  "HAVE_EIGEN"
+  "HAVE_GLEW"
+  "PANGO_DEFAULT_WIN_URI=\"wayland\""
+  "_LINUX_"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../"
+  "../include"
+  "/usr/include/eigen3"
+  "/usr/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../test/test"
+  "../test/test.pdb"
+  "CMakeFiles/test.dir/test/test_what.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
